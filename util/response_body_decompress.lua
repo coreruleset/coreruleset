8597863e09553ce1bd859de8dd6a4e95450c2f64
azurit <jozef@sudolsky.sk>
2021-01-05 17:51:28 +0100

    Fix typo

--- util/response_body_decompress.lua
+++ util/response_body_decompress.lua
@@ -3,6 +3,6 @@ require("zlib")
 function main()
 local f = zlib.inflate()
 local response_body_uncompressed = f(m.getvar("RESPONSE_BODY", "none"))
-m.setvar("tx.response_body_uncompressed", response_body_uncompressed)
+m.setvar("tx.response_body_decompressed", response_body_uncompressed)
 return nil
 end
