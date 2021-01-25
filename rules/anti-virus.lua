b01b14732941340eb879483f87faa152c4e94902
azurit <jozef@sudolsky.sk>
2021-01-25 17:47:25 +0100

    Additional fixies

--- rules/anti-virus.lua
+++ rules/anti-virus.lua
@@ -1,5 +1,7 @@
 require("m")
 
+-- Binary packing of numbers into big-endian 4 byte integer (>I4),
+-- added for compatibility with Lua version < 5.3.
 function clamav_pack_chunk_length(x)
 	local bytes = {}
 	for j = 1, 4 do
@@ -14,17 +16,21 @@ function main(filename)
 	local connect_type = m.getvar("tx.crs_anti-virus_clamav_connect_type")
 	if connect_type == "socket" then
 		module_name = "socket.unix"
-	else
+	elseif connect_type == "tcp" then
 		module_name = "socket"
+	else
+		m.log(2, string.format("ClamAV: invalid value '%s' for 'tx.crs_anti-virus_clamav_connect_type' in crs-setup.conf.", connect_type))
+		return nil
 	end
 	local ok, socket = pcall(require, module_name)
 	if not ok then
-		m.log(2, "ClamAV: lua-socket library not installed, please install it or disable 'tx.crs_anti-virus_protection_enable' in crs-setup.conf")
+		m.log(2, "ClamAV: lua-socket library not installed, please install it or disable 'tx.crs_anti-virus_protection_enable' in crs-setup.conf.")
 		return nil
 	end
 
 	local file_handle = io.open(filename, "r")
 	local file_size = file_handle:seek("end")
+	-- Empty file, nothing to scan.
 	if file_size == 0 then
 		return nil
 	elseif file_size > tonumber(m.getvar("tx.crs_anti-virus_clamav_max_file_size_bytes")) then
@@ -35,8 +41,10 @@ function main(filename)
 
 	sck = socket.tcp()
 	sck:settimeout(tonumber(m.getvar("tx.crs_anti-virus_clamav_network_timeout_seconds", "none")))
+	-- Connecting using unix socket file.
 	if connect_type == "socket" then
 		status, error = sck:connect(m.getvar("tx.crs_anti-virus_clamav_socket_file"))
+	-- Connecting using TCP/IP.
 	else
 		status, error = sck:connect(m.getvar("tx.crs_anti-virus_clamav_address", "none"), tonumber(m.getvar("tx.crs_anti-virus_clamav_port", "none")))
 	end
@@ -47,9 +55,13 @@ function main(filename)
 
 	sck:send("nINSTREAM\n")
 
+	-- Data needs to be streamed in chunks prefixed by binary packed chunk size.
+	-- Streaming is terminated by sending a zero-length chunk.
+	-- Chunk size is configurable but must NOT exceed StreamMaxLength defined in ClamAV configuration file.
 	while true do
 		local chunk = file_handle:read(chunk_size)
 		if chunk then
+			-- string.pack was introduced in Lua 5.3 but we need to support older versions.
 			--sck:send(string.pack(">I4", string.len(chunk)) .. chunk)
 			sck:send(clamav_pack_chunk_length(string.len(chunk)) .. chunk)
 		else
