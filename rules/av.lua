4de205a2fd8db4f80892060a3148e13c16020dc2
azurit <jozef@sudolsky.sk>
2021-01-25 09:20:58 +0100

    Added support for max file size, fixed typos, few improvements

--- rules/av.lua
+++ rules/av.lua
@@ -10,16 +10,11 @@ function clamav_pack_chunk_length(x)
 end
 
 function main(filename)
-	local connect_timeout = tonumber(m.getvar("tx.crs_anti-virus_clamav_chunk_size", "none"))
-	local chunk_size = tonumber(m.getvar("tx.crs_anti-virus_clamav_connect_timeout", "none"))
+	local chunk_size = tonumber(m.getvar("tx.crs_anti-virus_clamav_chunk_size", "none"))
 	local connect_type = m.getvar("tx.crs_anti-virus_clamav_connect_type")
-	local module_name = ""
 	if connect_type == "socket" then
-		socket_file = m.getvar("tx.crs_anti-virus_clamav_socket_file")
 		module_name = "socket.unix"
 	else
-		host = m.getvar("tx.crs_anti-virus_clamav_address", "none")
-		port = tonumber(m.getvar("tx.crs_anti-virus_clamav_port", "none"))
 		module_name = "socket"
 	end
 	local ok, socket = pcall(require, module_name)
@@ -32,31 +27,34 @@ function main(filename)
 	local file_size = file_handle:seek("end")
 	if file_size == 0 then
 		return nil
+	elseif file_size > tonumber(m.getvar("tx.crs_anti-virus_clamav_max_file_size_bytes")) then
+		m.log(2, string.format("ClamAV: Scan aborted, file is too big (see 'tx.crs_anti-virus_clamav_max_file_size_bytes' in crs-setup.conf), file size: %s bytes", file_size))
+		return nil
 	end
 	file_handle:seek("set", 0)
 
-	tcp = socket.tcp()
-	tcp:settimeout(connect_timeout)
+	sck = socket.tcp()
+	sck:settimeout(tonumber(m.getvar("tx.crs_anti-virus_clamav_network_timeout_seconds", "none")))
 	if connect_type == "socket" then
-		status, error = tcp:connect(socket_file)
+		status, error = sck:connect(m.getvar("tx.crs_anti-virus_clamav_socket_file"))
 	else
-		status, error = tcp:connect(host, port)
+		status, error = sck:connect(m.getvar("tx.crs_anti-virus_clamav_address", "none"), tonumber(m.getvar("tx.crs_anti-virus_clamav_port", "none")))
 	end
 	if not status then
 		m.log(2, string.format("ClamAV: Error connecting to antivirus: %s", error))
 		return nil
 	end
 
-	tcp:send("nINSTREAM\n")
+	sck:send("nINSTREAM\n")
 
 	while true do
 		local chunk = file_handle:read(chunk_size)
 		if chunk then
-			--tcp:send(string.pack(">I4", string.len(chunk)) .. chunk)
-			tcp:send(clamav_pack_chunk_length(string.len(chunk)) .. chunk)
+			--sck:send(string.pack(">I4", string.len(chunk)) .. chunk)
+			sck:send(clamav_pack_chunk_length(string.len(chunk)) .. chunk)
 		else
-			--tcp:send(string.pack(">I4", 0))
-			tcp:send(clamav_pack_chunk_length(0))
+			--sck:send(string.pack(">I4", 0))
+			sck:send(clamav_pack_chunk_length(0))
 			break
 		end
 	end
@@ -65,7 +63,7 @@ function main(filename)
 
 	local output = ""
 	while true do
-		local s, status, partial = tcp:receive()
+		local s, status, partial = sck:receive()
 		if s then
 			output = output .. s
 		elseif partial then
@@ -79,13 +77,14 @@ function main(filename)
 		end
 	end
 
-	tcp:close()
+	sck:close()
 
-        if output == "stream: OK" then
+	if output == "stream: OK" then
 		return nil
 	else
 		local virus_name = string.match(output, "^stream: (.+) FOUND$")
 		if virus_name then
+			m.setvar("tx.virus_name", virus_name)
 			return string.format("ClamAV: Virus found in uploaded file: %s", virus_name)
 		else
 			m.log2(string.format("ClamAV: Unknown response from antivirus: %s", output))
