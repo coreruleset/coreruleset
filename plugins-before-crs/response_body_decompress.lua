ac235fd75b7dbd4362648a442e0ea84203099a25
azurit <jozef@sudolsky.sk>
2021-01-28 09:49:25 +0100

    Documentation, response body decompress plugin

---
+++ plugins-before-crs/response_body_decompress.lua
@@ -0,0 +1,15 @@
+require("m")
+function main()
+	if pcall(require, "zlib") then
+		local f = zlib.inflate()
+		status, response_body_decompressed = pcall(f, m.getvar("RESPONSE_BODY", "none"))
+		if status then
+			m.setvar("tx.response_body_decompressed", response_body_decompressed)
+		else
+			m.log(2, "ERROR: decompression of response body failed")
+		end
+	else
+		m.log(2, "ERROR: lua-zlib library not installed, please install it or disable 'decompress_response_body' in crs-setup.conf")
+	end
+	return nil
+end
