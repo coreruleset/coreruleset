82e27775a791057906a63470dad1a3d2992db7bf
Max Leske <maxleske@gmail.com>
2021-06-22 20:36:34 +0200

    Replaced the original regexp-assemble.pl script with the v2 version.
    Added comments to explain what the additional Perl code does.
    
    Also added comments to regexp-cmdline.py, explaining it's use case
    to the uninitiated.

--- util/regexp-assemble/regexp-assemble.pl
+++ util/regexp-assemble/regexp-assemble.pl
@@ -15,6 +15,22 @@ use Regexp::Assemble;
 my $ra = Regexp::Assemble->new;
 while (<>)
 {
-  $ra->add($_);
+  # Handle possessive qualifiers
+  # https://rt.cpan.org/Public/Bug/Display.html?id=50228#txn-672717
+  # the code below does nearly the same thing as add(), which is enough for our pruposes
+
+  # parse an expression like `(a++|b)++|b` into an array of `["(a++|b)+", "+", "|", "b"]`
+  my $arr = $ra->lexstr($_);
+  for (my $n = 0; $n < $#$arr - 1; ++$n)
+  {
+    # find consecutive pairs where the first element ends with `+` and the last element is only `+`
+    if ($arr->[$n] =~ /\+$/ and $arr->[$n + 1] eq '+') {
+      # delete the second of the pair, concatenating it with the first element
+      $arr->[$n] .= splice(@$arr, $n + 1, 1);
+    }
+  }
+  # at this point the array looks like this: `["(a++|b)++", "|", "b"]`
+  # we now have what we would want to pass to add(), so add it
+  $ra->insert(@$arr);
 }
 print $ra->as_string() . "\n";
