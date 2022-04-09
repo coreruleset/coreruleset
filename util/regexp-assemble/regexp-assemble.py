4974102f691acb60b201a867e111cb6b48dc1ee2
Max Leske <maxleske@gmail.com>
2022-04-09 14:53:10 +0200

    Fixed merge errors in regexp-assemble.py

--- util/regexp-assemble/regexp-assemble.py
+++ util/regexp-assemble/regexp-assemble.py
@@ -9,96 +9,17 @@
 # the preprocessor_map. The key of the entry is the keyword used in the data file
 # and the value is the script you want to execute (expected to be in the lib directory).
 
-<<<<<<< HEAD
-import sys;
-import os;
-import fileinput
-import re;
-from subprocess import Popen, PIPE, TimeoutExpired
-||||||| 6692cb92
-import sys;
-import os;
-import fileinput;
-import re;
-from subprocess import Popen, PIPE, TimeoutExpired
-=======
 import os, re, sys
 import argparse
 import logging
 from typing import TypeVar
->>>>>>> unified-regex-utils
 
-<<<<<<< HEAD
-class Preprocessor(object):
-	def __init__(self, script_path, allowed_keywords, args):
-		for arg in args:
-			if arg not in allowed_keywords:
-				print(f'Keywoard "{arg}" not allowed in this context')
-				sys.exit(1)
-		effective_args = [arg for arg in args if allowed_keywords[arg]]
-		self.callout = [script_path] + effective_args
-		self._args = args
-		self._allowed_keywords = allowed_keywords
-||||||| 6692cb92
-class Preprocessor(object):
-	def __init__(self, script_path, args):
-		self.callout = [script_path] + args
-=======
 from lib.operators.assembler import Assembler
 from lib.operators.comparer import Comparer
 from lib.operators.updater import Updater
 from lib.context import Context
->>>>>>> unified-regex-utils
 
-<<<<<<< HEAD
-	def run(self, iterator):
-		preprocessed = self._preprocess(iterator, self._filter)
-		return self._postprocess(preprocessed)
-
-	def _preprocess(self, iterator, filter):
-		proc = Popen(self.callout, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		for line in filter(iterator):
-			proc.stdin.write(line.encode('utf-8'))
-		
-		errs = None
-		try:
-			outs, errs = proc.communicate(timeout=30)
-			if len(errs) > 0:
-				sys.stderr.write(f'Callout failed: {self.callout}')
-				sys.stderr.writelines(errs.decode('utf-8'))
-				sys.exit(1)
-		except TimeoutExpired:
-			proc.kill()
-			print(f'Callout timed out: {self.callout}')
-			print('Stderr: ' + errs.decode('utf-8'))
-			sys.exit(1)
-		return outs.decode('utf-8').splitlines()
-	
-	def _postprocess(self, output):
-		return output
-	
-||||||| 6692cb92
-	def run(self, iterator):
-		return self._preprocess(iterator, self._filter)
-
-	def _preprocess(self, iterator, filter):
-		proc = Popen(self.callout, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		for line in filter(iterator):
-			proc.stdin.write(line.encode('utf-8'))
-		
-		errs = None
-		try:
-			outs, errs = proc.communicate(timeout=30)
-		except TimeoutExpired:
-			proc.kill()
-			print(f'Callout timed out: {self.callout}')
-			print('Stderr: ' + errs.decode('utf-8'))
-			sys.exit(1)
-		return outs.decode('utf-8').splitlines()
-	
-=======
 S = TypeVar("S", bound="argparse._SubParserAction[argparse.ArgumentParser]")
->>>>>>> unified-regex-utils
 
 
 def build_args_parser() -> argparse.ArgumentParser:
@@ -120,118 +41,7 @@ def build_args_parser() -> argparse.ArgumentParser:
 
     return parser
 
-	def _postprocess(self, output):
-		if 'prefix' in self._args:
-			assert len(output) <= 1
-			return ['##!^ ' + line for line in output]
-		elif 'suffix' in self._args:
-			assert len(output) <= 1
-			return ['##!$ ' + line for line in output]
-		else:
-			return output
 
-
-<<<<<<< HEAD
-
-script_directory = os.path.dirname(__file__)
-lib_directory = os.path.join(script_directory, 'lib')
-preprocessor_map = {
-	# directive name : (processor type, path to script, allowed keywords (True: pass to script))
-	'cmdline': (BlockPreprocessor, os.path.join(lib_directory, 'regexp-cmdline.py'), {'windows': True, 'unix': True}),
-	'assemble': (BlockPreprocessor, os.path.join(script_directory, 'regexp-assemble.py'), {'prefix': False, 'suffix': False})
-}
-preprocessor_regex = re.compile(r'^##!>\s*(.*)')
-# prefix, suffix, flags, block start block end
-special_comment_markers = '^$+><'
-simple_comment_regex = re.compile(r'^##![^' + special_comment_markers + r'].*')
-
-def detect_preprocessor(line):
-	match = preprocessor_regex.match(line)
-	if match is None:
-		return
-		
-	definition = match.group(1).split()
-	try:
-		processor_type, script_path, allowed_keywords = preprocessor_map[definition[0]]
-		return processor_type(script_path, allowed_keywords, definition[1:])
-	except KeyError:
-		print(f'No processor found for {definition}')
-		sys.exit(1)
-
-def _is_simple_comment(line):
-	return simple_comment_regex.match(line) is not None
-
-def preprocess(lines):
-	iterator = lines.__iter__()
-	final_lines = []
-	for processor_type in (LinePreprocessor, BlockPreprocessor, FilePreprocessor):
-		transformed_lines = []
-		line = next(iterator, None)
-		while line is not None:
-			if (len(line.strip()) == 0 or _is_simple_comment(line)):
-				line = next(iterator, None)
-				continue
-			processor = detect_preprocessor(line)
-			if processor is None or not isinstance(processor, processor_type):
-				transformed_lines.append(line)
-				line = next(iterator, None)
-				continue
-			transformed_lines += processor.run(iterator)
-			line = next(iterator, None)
-
-		final_lines = transformed_lines
-		iterator = final_lines.__iter__()
-	return final_lines
-||||||| 6692cb92
-
-script_directory = os.path.dirname(__file__)
-lib_directory = os.path.join(script_directory, 'lib')
-preprocessor_map = {
-	'cmdline': (FilePreprocessor, os.path.join(lib_directory, 'regexp-cmdline.py')),
-}
-preprocessor_regex = re.compile(r'^##!>\s*(.*)')
-# prefix, suffix, flags, block start block end
-special_comment_markers = '^$+><'
-simple_comment_regex = re.compile(r'^##![^' + special_comment_markers + r'].*')
-
-def detect_preprocessor(line):
-	match = preprocessor_regex.match(line)
-	if match is None:
-		return
-		
-	definition = match.group(1).split()
-	try:
-		processor_type, script_path = preprocessor_map[definition[0]]
-		return processor_type(script_path, definition[1:])
-	except KeyError:
-		print(f'No processor found for {definition}')
-		sys.exit(1)
-
-def _is_simple_comment(line):
-	return simple_comment_regex.match(line) is not None
-
-def preprocess(lines):
-	iterator = lines.__iter__()
-	final_lines = []
-	for processor_type in (LinePreprocessor, BlockPreprocessor, FilePreprocessor):
-		transformed_lines = []
-		line = next(iterator, None)
-		while line is not None:
-			if (_is_simple_comment(line)):
-				line = next(iterator, None)
-				continue
-			processor = detect_preprocessor(line)
-			if processor is None or not isinstance(processor, processor_type):
-				transformed_lines.append(line)
-				line = next(iterator, None)
-				continue
-			transformed_lines += processor.run(iterator)
-			line = next(iterator, None)
-
-		final_lines = transformed_lines
-		iterator = final_lines.__iter__()
-	return final_lines
-=======
 def build_generate_args_parser(subparsers: S):
     parser = subparsers.add_parser(
         "generate",
@@ -251,7 +61,6 @@ def build_generate_args_parser(subparsers: S):
     """,
     )
     parser.set_defaults(func=handle_generate)
->>>>>>> unified-regex-utils
 
 
 def build_update_args_parser(subparsers: S):
@@ -281,22 +90,6 @@ def build_update_args_parser(subparsers: S):
     )
     parser.set_defaults(func=handle_update)
 
-<<<<<<< HEAD
-	if errs:
-		sys.stderr.write('Failed to assemble regex\n')
-		sys.stderr.write('Stderr: ' + errs.decode('utf-8') + '\n')
-		sys.exit(1)
-
-	sys.stdout.write(outs.decode('utf-8'))
-||||||| 6692cb92
-	if errs:
-		print('Failed to assemble regex')
-		print('Stderr: ' + errs.decode('utf-8'))
-		sys.exit(1)
-	
-	sys.stdout.write(outs.decode('utf-8'))
-=======
->>>>>>> unified-regex-utils
 
 def build_compare_args_parser(subparsers: S):
     parser = subparsers.add_parser(
@@ -329,28 +122,7 @@ def build_compare_args_parser(subparsers: S):
     )
     parser.set_defaults(func=handle_compare)
 
-<<<<<<< HEAD
-	lines = preprocess(iterator)
-	if len(lines) == 0:
-		sys.stderr.write('No input. Either pass a filename, a rule id or pipe data to the script\n')
-		sys.exit(1)
-	assemble(lines)
-||||||| 6692cb92
-	lines = preprocess(iterator)
-	if len(lines) == 0:
-		print('No input. Either pass a filename, a rule id or pipe data to the script')
-		sys.exit(1)
-	assemble(lines)
-=======
->>>>>>> unified-regex-utils
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-	run()
-||||||| 6692cb92
-if __name__ == '__main__':
-	run()
-=======
 def handle_generate(namespace: argparse.Namespace):
     context = create_context()
     assembler = Assembler(context)
@@ -455,4 +227,3 @@ if __name__ == "__main__":
         namespace.func(namespace)
     else:
         parser.print_help()
->>>>>>> unified-regex-utils
