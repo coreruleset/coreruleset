8b76945f4be106546151b82feee61862e3b74228
themiddleblue <theMiddleBlue@users.noreply.github.com>
2022-11-25 14:16:56 +0100

    fix(util): fix fp-finder/spell.sh using shellcheck

--- util/fp-finder/spell.sh
+++ util/fp-finder/spell.sh
@@ -4,14 +4,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 RULES_DIR="$SCRIPT_DIR/../../rules/"
 
-for datafile in $(ls -1 "$RULES_DIR"*.data); do
+for datafile in "$RULES_DIR"*.data; do
     DATAFILE_NAME=${datafile##*/}
     
     echo "-> checking $DATAFILE_NAME"
 
-    for word in $(egrep '^[a-z]+$' $datafile | uniq | sort); do
-        IS_NOT_ENGLISH=$(echo $word | spell | wc -l)
-        if [ $IS_NOT_ENGLISH -lt 1 ]; then
+    for word in $(grep -E '^[a-z]+$' "$datafile" | uniq | sort); do
+        IS_NOT_ENGLISH=$(echo "$word" | spell | wc -l)
+        if [ "$IS_NOT_ENGLISH" -lt 1 ]; then
             echo "   \`- found English word: $word"
         fi
     done
