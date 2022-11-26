2e596f472a7de263ad6bef3983aff865d59e1027
theMiddle <theMiddleBlue@users.noreply.github.com>
2022-11-26 02:34:20 +0100

    Update util/fp-finder/spell.sh
    Co-authored-by: Max Leske <th3s3ion@gmail.com>

--- util/fp-finder/spell.sh
+++ util/fp-finder/spell.sh
@@ -11,7 +11,7 @@ for datafile in "$RULES_DIR"*.data; do
 
     for word in $(grep -E '^[a-z]+$' "$datafile" | uniq | sort); do
         IS_NOT_ENGLISH=$(echo "${word}" | spell | wc -l)
-        if [ "$IS_NOT_ENGLISH" -lt 1 ]; then
+        if [ "${IS_NOT_ENGLISH}" -lt 1 ]; then
             echo "   \`- found English word: $word"
         fi
     done
