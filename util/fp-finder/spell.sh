11ea9999667b7ca1be600950e5f06a8418d46ce3
franbuehler <franziska.buehler.schmocker@gmail.com>
2023-06-07 18:42:42 +0200

    Extend spell.sh

--- util/fp-finder/spell.sh
+++ util/fp-finder/spell.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# This script finds common English words in .data files (if no argument provided).
+# Or it finds english words in the provided input file (one argument).
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RULES_DIR="${SCRIPT_DIR}/../../rules/"
 
-for datafile in "$RULES_DIR"*.data; do
-    DATAFILE_NAME=${datafile##*/}
-    
-    echo "-> checking ${DATAFILE_NAME}"
-
-    for word in $(grep -E '^[a-z]+$' "${datafile}" | uniq | sort); do
+# If no argument provided:
+if [ $# -eq 0 ]
+  then
+    for datafile in "$RULES_DIR"*.data; do
+      DATAFILE_NAME=${datafile##*/}
+      echo "-> checking ${DATAFILE_NAME}"
+      for word in $(grep -E '^[a-z]+$' "${datafile}" | uniq | sort); do
         IS_NOT_ENGLISH=$(echo "${word}" | spell | wc -l)
         if [ "${IS_NOT_ENGLISH}" -lt 1 ]; then
             echo "   \`- found English word: ${word}"
         fi
-    done
-
+      done
     echo ""
-done
+  done
+# If argument provided, check this file:
+else
+  datafile=$SCRIPT_DIR/../../$1
+  DATAFILE_NAME=${datafile##*/}
+  for word in $(grep -E '[a-z]+$' "${datafile}" | grep -vE [#@\\] | uniq | sort); do
+    IS_NOT_ENGLISH=$(echo "${word}" | spell | wc -l)
+    if [ "${IS_NOT_ENGLISH}" -lt 1 ]; then
+      echo "   \`- found English word: ${word}"
+    fi
+  done
+  echo ""
+fi
