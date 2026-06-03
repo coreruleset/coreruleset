coreruleset/coreruleset

commit fa7f8f1ae4ca658a59a0221d3b81209692099556

fix 941170 (#2292)

rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf
@@ -210,7 +210,7 @@ SecRule REQUEST_COOKIES|!REQUEST_COOKIES:/__utm/|REQUEST_COOKIES_NAMES|REQUEST_H
 #
 # [NoScript InjectionChecker] Attributes injection
 #
-SecRule REQUEST_COOKIES|!REQUEST_COOKIES:/__utm/|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST_HEADERS:Referer|ARGS_NAMES|ARGS|XML:/* "@rx (?i)(?:\W|^)(?:javascript:(?:[\s\S]+[=\\\(\[\.<]|[\s\S]*?(?:\bname\b|[\\\\][ux]\d))|data:(?:(?:[a-z]\w+/\w[\w+-]+\w)?[;,]|[\s\S]*?;[\s\S]*?\b(?:base64|charset=)|[\s\S]*?,[\s\S]*?<[\s\S]*?\w[\s\S]*?>))|@\W*?i\W*?m\W*?p\W*?o\W*?r\W*?t\W*?(?:/\*[\s\S]*?)?(?:[\"']|\W*?u\W*?r\W*?l[\s\S]*?\()|\W*?-\W*?m\W*?o\W*?z\W*?-\W*?b\W*?i\W*?n\W*?d\W*?i\W*?n\W*?g[\s\S]*?:[\s\S]*?\W*?u\W*?r\W*?l[\s\S]*?\(" \
+SecRule REQUEST_COOKIES|!REQUEST_COOKIES:/__utm/|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST_HEADERS:Referer|ARGS_NAMES|ARGS|XML:/* "@rx (?i)(?:\W|^)(?:javascript:(?:[\s\S]+[=\\\(\[\.<]|[\s\S]*?(?:\bname\b|[\\\\][ux]\d))|data:(?:(?:[a-z]\w+/\w[\w+-]+\w)?[;,]|[\s\S]*?;[\s\S]*?\b(?:base64|charset=)|[\s\S]*?,[\s\S]*?<[\s\S]*?\w[\s\S]*?>))|@\W*?i\W*?m\W*?p\W*?o\W*?r\W*?t\W*?(?:/\*[\s\S]*?)?(?:[\"']|\W*?u\W*?r\W*?l[\s\S]*?\()|[^-]*?-\W*?m\W*?o\W*?z\W*?-\W*?b\W*?i\W*?n\W*?d\W*?i\W*?n\W*?g[^:]*?:\W*?u\W*?r\W*?l[\s\S]*?\(" \
     "id:941170,\
     phase:2,\
     block,\

commit 721e85c2298f6651028fc906b3d1a9c16cdd554b

refactor: create 941170 `.ra` file (#4493)
* refactor: create 941170 `.ra` file

* refactor: decompose 941170 `.ra` into sub-assemblies

* fix(941170): use . over [\s\S] and fix moz-binding perf regression

replace [\s\S] with . in the .ra so crs-toolchain can generate a
PCRE-compatible pattern; [\s\S] expanded to [\x00-\x{10ffff}], which
fails toolchain validation and PCRE 8-bit compilation. . is
dotall-equivalent under modsec2-apache and modsec3-nginx (verified:
. matches newlines in both engines).

also revert the moz-binding branch quantifier [^-]*?- back to \W*?-,
the PCRE backtracking regression introduced in #2292 (see #4633), and
add test 6 covering the moz-binding branch, which had no coverage.

* Apply suggestions from code review

Co-authored-by: Max Leske <250711+theseion@users.noreply.github.com>

* chore(formatting): auto fixes from pre-commit hooks

for more information, see https://pre-commit.ci

---------

Co-authored-by: Max Leske <250711+theseion@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

regex-assembly/941170.ra
@@ -1,0 +1,79 @@
+##! Please refer to the documentation at
+##! https://coreruleset.org/docs/development/regex_assembly/.
+
+##! NoScript InjectionChecker: Attribute Injection.
+##! Detects javascript: and data: URI schemes, CSS @import, and
+##! -moz-binding injection attempts.
+
+##!+ i
+
+##! 'name' keyword or Unicode/hex escape sequence
+##!> assemble
+  \bname\b
+  \x5c[ux]\d
+  ##!=< js-name-or-escape
+##!<
+
+##! javascript: payload - match followed by name keyword or escape
+##!> assemble
+  .*?
+  ##!=> js-name-or-escape
+  ##!=< js-payload-name-match
+##!<
+
+##! data: URI encoding keywords
+##!> assemble
+  base64
+  charset=
+  ##!=< data-encoding-keyword
+##!<
+
+##! data: payload with encoding keyword
+##!> assemble
+  .*?;.*?\b
+  ##!=> data-encoding-keyword
+  ##!=< data-payload-encoding
+##!<
+
+##! CSS @import target (quote or url function)
+##!> assemble
+  ["']
+  \W*?u\W*?r\W*?l.*?\(
+  ##!=< import-target
+##!<
+
+##! javascript: URI - payload with special chars
+##!> assemble
+  (?:\W|^)javascript:.+[=\x5c\(\[\.<]
+##!<
+
+##! javascript: URI - payload with name keyword or escape
+##!> assemble
+  (?:\W|^)javascript:
+  ##!=> js-payload-name-match
+##!<
+
+##! data: URI - optional MIME type followed by semicolon or comma
+##!> assemble
+  (?:\W|^)data:(?:[a-z]\w+/\w[\w+-]+\w)?[;,]
+##!<
+
+##! data: URI - encoding keyword (base64 or charset)
+##!> assemble
+  (?:\W|^)data:
+  ##!=> data-payload-encoding
+##!<
+
+##! data: URI - HTML-like content
+##!> assemble
+  (?:\W|^)data:.*?,.*?<.*?\w.*?>
+##!<
+
+##! CSS @import
+##!> assemble
+  @\W*?i\W*?m\W*?p\W*?o\W*?r\W*?t\W*?(?:/\*.*?)?
+  ##!=> import-target
+##!<
+
+##! -moz-binding
+-\W*?m\W*?o\W*?z\W*?-\W*?b\W*?i\W*?n\W*?d\W*?i\W*?n\W*?g[^:]*?:\W*?u\W*?r\W*?l.*?\(

rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf
@@ -271,7 +271,12 @@ SecRule REQUEST_COOKIES|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST
 #
 # [NoScript InjectionChecker] Attributes injection
 #
-SecRule REQUEST_COOKIES|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST_HEADERS:Referer|ARGS_NAMES|ARGS|REQUEST_FILENAME|XML:/* "@rx (?i)(?:\W|^)(?:javascript:(?:[\s\S]+[=\x5c\(\[\.<]|[\s\S]*?(?:\bname\b|\x5c[ux]\d))|data:(?:(?:[a-z]\w+/\w[\w+-]+\w)?[;,]|[\s\S]*?;[\s\S]*?\b(?:base64|charset=)|[\s\S]*?,[\s\S]*?<[\s\S]*?\w[\s\S]*?>))|@\W*?i\W*?m\W*?p\W*?o\W*?r\W*?t\W*?(?:/\*[\s\S]*?)?(?:[\"']|\W*?u\W*?r\W*?l[\s\S]*?\()|[^-]*?-\W*?m\W*?o\W*?z\W*?-\W*?b\W*?i\W*?n\W*?d\W*?i\W*?n\W*?g[^:]*?:\W*?u\W*?r\W*?l[\s\S]*?\(" \
+# Regular expression generated from regex-assembly/941170.ra.
+# To update the regular expression run the following shell script
+# (consult https://coreruleset.org/docs/development/regex_assembly/ for details):
+#   crs-toolchain regex update 941170
+#
+SecRule REQUEST_COOKIES|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST_HEADERS:Referer|ARGS_NAMES|ARGS|REQUEST_FILENAME|XML:/* "@rx (?i)(?:[^0-9A-Z_a-z]|^)(?:javascript:(?:.+[\(\.<=\[\x5c]|.*?(?:\bname\b|\x5c[ux][0-9]))|data:(?:(?:[a-z][0-9A-Z_a-z]+/[0-9A-Z_a-z][\+\-0-9A-Z_a-z]+[0-9A-Z_a-z])?[,;]|.*?(?:;.*?\b(?:base64|charset=)|,.*?<.*?[0-9A-Z_a-z].*?>)))|@[^0-9A-Z_a-z]*?i[^0-9A-Z_a-z]*?m[^0-9A-Z_a-z]*?p[^0-9A-Z_a-z]*?o[^0-9A-Z_a-z]*?r[^0-9A-Z_a-z]*?t[^0-9A-Z_a-z]*?(?:/\*.*?)?(?:[\"']|[^0-9A-Z_a-z]*?u[^0-9A-Z_a-z]*?r[^0-9A-Z_a-z]*?l.*?\()|-[^0-9A-Z_a-z]*?m[^0-9A-Z_a-z]*?o[^0-9A-Z_a-z]*?z[^0-9A-Z_a-z]*?-[^0-9A-Z_a-z]*?b[^0-9A-Z_a-z]*?i[^0-9A-Z_a-z]*?n[^0-9A-Z_a-z]*?d[^0-9A-Z_a-z]*?i[^0-9A-Z_a-z]*?n[^0-9A-Z_a-z]*?g[^:]*?:[^0-9A-Z_a-z]*?u[^0-9A-Z_a-z]*?r[^0-9A-Z_a-z]*?l.*?\(" \
     "id:941170,\
     phase:2,\
     block,\

tests/regression/tests/REQUEST-941-APPLICATION-ATTACK-XSS/941170.yaml
@@ -84,3 +84,19 @@ tests:
         output:
           log:
             expect_ids: [941170]
+  - test_id: 6
+    desc: "CSS -moz-binding attribute injection (covers the moz-binding branch)"
+    stages:
+      - input:
+          dest_addr: 127.0.0.1
+          method: GET
+          port: 80
+          uri: "/get?style=-moz-binding%3Aurl%28http%3A%2F%2Fexample.com%2Fxss.xml%23xss%29"
+          headers:
+            Accept: "*/*"
+            User-Agent: "OWASP CRS test agent"
+            Host: localhost
+          version: "HTTP/1.1"
+        output:
+          log:
+            expect_ids: [941170]